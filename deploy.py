d97cc4d38d4073172e6c70ad6e3a1e20d2313a33
adabru <b.brunnmeier@gmx.de>
2023-12-02 10:28:28 +0100

    webhooks

--- deploy.py
+++ deploy.py
@@ -2,14 +2,23 @@ from getpass import getuser
 from pyinfra.operations import pacman, systemd, files, server
 from pyinfra import config
 
+# load .env
+with open(".env", "r") as fh:
+    env = dict(
+        tuple(line.replace("\n", "").split("="))
+        for line in fh.readlines()
+        if not line.startswith("#")
+    )
 
 config.SUDO = True
+config.USE_SUDO_PASSWORD = env["SUDO_PASSWORD"]
 admin = getuser()
 
 
-pacman.packages(name="Install nginx.", packages=["nginx"])
-server.user(name="Add http to admin group.", user="http", groups=[admin])
+pacman.packages(name="Install packages.", packages=["nginx", "webhook"])
 
+# nginx
+server.user(name="Add http to admin group.", user="http", groups=[admin])
 files.put(
     name="Update nginx.config.",
     src="nginx/nginx.conf",
@@ -18,7 +27,6 @@ files.put(
     group="root",
     mode="644",
 )
-
 systemd.service(
     name="Update nginx service.",
     service="nginx",
@@ -26,13 +34,32 @@ systemd.service(
     enabled=True,
     reloaded=True,
 )
-
-files.directory(name="Open home-directory to group.", path=f"/home/{admin}", mode="750")
-
+files.directory(
+    name="Open home-directory to group/nginx.", path=f"/home/{admin}", mode="750"
+)
 
 # filesharing
 files.sync(
-    name="Create folder for filesharing",
+    name="Create folder for filesharing.",
     dest=f"/home/{admin}/filesharing",
     src="filesharing",
+)
+
+# webhooks
+files.template(
+    name="Update webhook config.",
+    src="webhook/hooks.json.j2",
+    dest="/etc/webhook/hooks.json",
+    user="root",
+    group="root",
+    mode="644",
+    __admin=admin,
+    __env=env,
+)
+systemd.service(
+    name="Update webhook service.",
+    service="webhook",
+    running=True,
+    enabled=True,
+    restarted=True,
 )
